7d9263e02d416971fc48bfd270c0464256726b4a
Jordi Sabaté <3697530+zarpilla@users.noreply.github.com>
2025-07-30 18:41:18 +0200

    changed to nodemailer

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,9 +1,23 @@
 export default ({ env }) => ({
   email: {
     config: {
-      provider: "sendgrid",
+      // provider: "sendgrid",
+      // providerOptions: {
+      //   apiKey: env("SENDGRID_API_KEY"),
+      // },
+      // settings: {
+      //   defaultFrom: env("EMAIL_FROM"),
+      //   defaultReplyTo: env("EMAIL_FROM"),
+      // },
+      provider: "@strapi/provider-email-nodemailer",
       providerOptions: {
-        apiKey: env("SENDGRID_API_KEY"),
+        host: env("SMTP_HOST") || "localhost",
+        port: env("SMTP_PORT") || 587,
+        auth: {
+          user: env("SMTP_USER"),
+          pass: env("SMTP_PASS"),
+        },
+        secure: env("SMTP_SECURE") === "true", // true for 465, false for other ports
       },
       settings: {
         defaultFrom: env("EMAIL_FROM"),
